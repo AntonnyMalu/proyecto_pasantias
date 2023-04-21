27d9deef3e4748961e69f2d9bb384438657e00f2
AntonnyMalu <gabrielmalu15@gmail.com.com>
2023-04-21 11:08:08 -0400

    database nomina

--- mysql/table_nomina.sql
+++ mysql/table_nomina.sql
@@ -19,52 +19,6 @@
 CREATE DATABASE IF NOT EXISTS `atencion` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `atencion`;
 
--- Volcando estructura para tabla atencion.casos
-CREATE TABLE IF NOT EXISTS `casos` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `personas_id` int NOT NULL,
-  `fecha` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL DEFAULT '',
-  `hora` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL DEFAULT '',
-  `donativo` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL,
-  `tipo` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL,
-  `status` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci DEFAULT NULL,
-  `observacion` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL,
-  `band` int NOT NULL DEFAULT '1',
-  `created_at` date DEFAULT NULL,
-  `updated_at` date DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
-
--- Volcando datos para la tabla atencion.casos: ~0 rows (aproximadamente)
-
--- Volcando estructura para tabla atencion.firmantes
-CREATE TABLE IF NOT EXISTS `firmantes` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL,
-  `cargo` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL,
-  `created_at` date DEFAULT NULL,
-  `updated_at` date DEFAULT NULL,
-  `band` int NOT NULL DEFAULT '1',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
-
--- Volcando datos para la tabla atencion.firmantes: ~0 rows (aproximadamente)
-
--- Volcando estructura para tabla atencion.instituciones
-CREATE TABLE IF NOT EXISTS `instituciones` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `rif` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL,
-  `nombre` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL,
-  `telefono` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL,
-  `direccion` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL,
-  `band` int NOT NULL DEFAULT '1',
-  `created_at` date DEFAULT NULL,
-  `updated_at` date DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
-
--- Volcando datos para la tabla atencion.instituciones: ~5 rows (aproximadamente)
-
 -- Volcando estructura para tabla atencion.nomina
 CREATE TABLE IF NOT EXISTS `nomina` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
@@ -353,67 +307,6 @@ INSERT INTO `nomina` (`id`, `nombre`, `ubicacion_geografica`, `ubicacion_adminis
 	(267, 'VASQUEZ ROJAS CARLOS JAVIER', 'TIENDA', 'ROSCIO', '30.276.005', 'MANTENIMIENTO', 1, '', '', '', '0000-00-00', '0000-00-00'),
 	(268, 'APARICIO FARFAN MARIANGEL NAZARETH', 'TIENDA', 'CAMAGUAN', '30.439.081', 'PASILLERO (MUN CAMAGUAN)', 1, '', '', '', '0000-00-00', '0000-00-00'),
 	(269, 'HERNANDEZ PEREZ JOSE RAFAEL ', 'TIENDA', 'ROSCIO', '30.961.469', 'SEGURIDAD', 1, '', '', '', '0000-00-00', '0000-00-00');
-
--- Volcando estructura para tabla atencion.oficios
-CREATE TABLE IF NOT EXISTS `oficios` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `instituciones_id` int NOT NULL,
-  `personas_id` int NOT NULL,
-  `fecha` date NOT NULL,
-  `requerimientos` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL,
-  `status` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci DEFAULT NULL,
-  `band` int NOT NULL DEFAULT '1',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
-
--- Volcando datos para la tabla atencion.oficios: ~2 rows (aproximadamente)
-
--- Volcando estructura para tabla atencion.personas
-CREATE TABLE IF NOT EXISTS `personas` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `cedula` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL,
-  `nombre` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL,
-  `telefono` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci DEFAULT NULL,
-  `direccion` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL,
-  `band` int NOT NULL DEFAULT '1',
-  `created_at` date DEFAULT NULL,
-  `updated_at` date DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
-
--- Volcando datos para la tabla atencion.personas: ~0 rows (aproximadamente)
-
--- Volcando estructura para tabla atencion.productos
-CREATE TABLE IF NOT EXISTS `productos` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `casos_id` int NOT NULL,
-  `producto` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL,
-  `cantidad` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL,
-  `band` int NOT NULL DEFAULT '1',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
-
--- Volcando datos para la tabla atencion.productos: ~0 rows (aproximadamente)
-
--- Volcando estructura para tabla atencion.users
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `email` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_spanish_ci NOT NULL,
-  `password` text CHARACTER SET utf8mb3 COLLATE utf8mb3_spanish_ci NOT NULL,
-  `name` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_spanish_ci NOT NULL,
-  `role` int NOT NULL DEFAULT '0',
-  `band` int NOT NULL DEFAULT '1',
-  `created_at` date DEFAULT NULL,
-  `updated_at` date DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_spanish_ci;
-
--- Volcando datos para la tabla atencion.users: ~4 rows (aproximadamente)
-INSERT INTO `users` (`id`, `email`, `password`, `name`, `role`, `band`, `created_at`, `updated_at`) VALUES
-	(1, 'leothan522@gmail.com', '$2y$10$Vuukyxuv6haC.nQhAWK3oeGLwAq8POECeyx0eQOAO3JpVuw97pFHm', 'Yonathan Castillo', 100, 1, '2023-01-22', '2023-01-22'),
-	(2, 'gabrielmalu15@gmail.com', '$2y$10$tNST6nHuXNdeT/v3j/WCPeJvaBPGBgdRgNUCHk2QFJ7vJ79hv6PPi', ' Antonny Maluenga', 100, 1, '2023-01-22', '2023-04-14'),
-	(3, 'milexitorrealba536@gmail.com', '$2y$10$kOKftSQUdcGWHFwjou.t1OjHBx1l10SjME65dbKnQlyE.p.2xhDBK', 'Milexi Torrealba', 2, 1, '2023-03-31', '2023-03-31'),
-	(4, 'karleidysalazar@gmail.com', '$2y$10$9Qse5RDa4LaTNQub/COk3eI2LX7pgxzfRvFtQMWLbQyrTprnmSek2', 'Karleidy Salazar', 2, 1, '2023-03-31', '2023-03-31');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
