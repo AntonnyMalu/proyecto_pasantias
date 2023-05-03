dc0bae7a555a2bf3ba6481e9cc7829688959174c
Antonny Maluenga <gabrielmalu15@gmail.com>
2023-05-03 13:57:18 -0400

    Prueba Token

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 $url = $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"];
-//echo $url."web";
+//echo $url."web" Prueba;
 header("Status: 301 Moved Permanently");
 header("Location: admin");
 exit;
