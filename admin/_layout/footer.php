3d2e991f549cdeec7156777be022ffece7fc456d
Jose Infante <infantelarajosealejandro@gmail.com>
2025-01-31 10:51:05 -0400

    fooster actualizado

--- admin/_layout/footer.php
+++ admin/_layout/footer.php
@@ -1,7 +1,7 @@
 <footer class="sticky-footer ">
     <div class="container my-auto">
         <div class="copyright text-center my-auto text-gray-800">
-            <span><!--Copyright -->&copy;2023 Alimentos del Guárico S.A. (ALGUARISA)</span>
+            <span><!--Copyright -->&copy;2023 - 2025 Alimentos del Guárico S.A. (ALGUARISA)</span>
         </div>
     </div>
 </footer>
